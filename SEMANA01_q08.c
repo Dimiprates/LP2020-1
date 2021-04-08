91ddff3e6e6d7b2d17072657d9e30629f85cae5d
GabrielPrates <>
2021-04-08 22:29:39 +0000

    a

---
+++ SEMANA01_q08.c
@@ -0,0 +1,14 @@
+//#include <stdio.h>
+
+//int main() {
+//	float x,y;
+//	scanf("%f %f",&x,&y);
+//	if(x-11>58 && x+11<98 && y-11>204 && y+11<244 || x-11>750 && x+11<790 && y-11>204 && y+11<244 ){
+//		printf("GOLACO");
+//	}else
+//		if(x-11<58 || x+11>790 || y+11>=244){
+//			printf("UHHH");
+//		}else
+//			printf("GOL");
+//	return 0;
+//}
---
+++ SEMANA01_q09.c
@@ -0,0 +1,42 @@
+//#include <stdio.h>
+
+//int main() {
+//	int valor,n100,n50,n20,n10,n5,n2,n1;
+//	scanf("%i",&valor);
+//	n100= valor/100;
+//	valor=valor%100;
+//	n50=valor/50;
+//	valor=valor%50;
+//	n20=valor/20;
+//	valor=valor%20;
+//	n10=valor/10;
+//	valor=valor%10;
+//	n5=valor/5;
+//	valor=valor%5;
+//	n2=valor/2;
+//	valor=valor%2;
+//	n1=valor%2;
+//	if(n100>0){
+//		printf("%i notas de R$ 100\n",n100);
+//	}
+//	if(n50>0){
+//		printf("%i notas de R$ 50\n",n50);
+//	}
+//	if(n20>0){
+//		printf("%i notas de R$ 20\n",n20);
+//	}
+//	if(n10>0){
+//		printf("%i notas de R$ 10\n",n10);
+//	}
+//	if(n5>0){
+//		printf("%i notas de R$ 5\n",n5);
+//	}
+//	if(n2>0){
+//		printf("%i notas de R$ 2\n",n2);
+//	}
+//	if(n1>0){
+//		printf("%i notas de R$ 1\n",n1);
+//	}
+		
+//	return 0;
+//}
