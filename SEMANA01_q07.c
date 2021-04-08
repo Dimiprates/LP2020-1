8728c1679f744a3024033d38cbf42227033611f2
GabrielPrates <>
2021-04-08 22:22:44 +0000

    a

---
+++ SEMANA01_q07.c
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+
+//int main() {
+//	int valor,n100,n50,n20,n10,n5,n2,n1;
+//	scanf("%i",&valor);
+//	n100= valor/100;
+//	valor=valor%100;
+//	n50=valor/50;
+//	valor=valor%50;
+//	n20=valor/20;
+//	valor=valor%20;
+//	n10=valor/10;
+//	valor=valor%10;
+//	n5=valor/5;
+//	valor=valor%5;
+//	n2=valor/2;
+//	valor=valor%2;
+//	n1=valor%2;
+//	printf("%i notas de R$ 100\n%i notas de R$ 50\n%i notas de R$ 20\n%i notas de R$ 10\n%i notas de R$ 5\n%i notas de R$ 2\n%i notas de R$ 1\n",n100,n50,n20,n10,n5,n2,n1);	
+//	return 0;
+//}
