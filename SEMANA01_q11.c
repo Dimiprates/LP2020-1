0e8471982639b51147ab82ef5943829d7305d9ec
GabrielPrates <>
2021-04-09 04:21:42 +0000

    a

---
+++ SEMANA01_q11.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+
+//int main() {
+//	int dv,n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,impar,par;
+//	scanf("%i %i %i %i %i %i %i %i %i %i %i %i %i",&dv,&n1,&n2,&n3,&n4,&n5,&n6,&n7,&n8,&n9,&n10,&n11,&n12);
+//	impar=n1+n3+n5+n7+n9+n11;
+//	impar*=3;
+//	par=n2+n4+n6+n8+n10+n12;
+//	impar+=par;
+//	if((impar+dv)%10==0){
+//		printf("S \n");
+//	}else{
+//printf("N \n");
+//    }
+		
+//	return 0;
+//}
