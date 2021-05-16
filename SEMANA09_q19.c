f78ed1bf52bed0d1354798fde3239aa3790ed1ef
GabrielPrates <>
2021-05-16 22:27:04 +0000

    a

---
+++ SEMANA09_q19.c
@@ -0,0 +1,62 @@
+/*#include <stdio.h>
+ 
+int main(){
+  int N,l,c;
+ 
+  char tipo;
+  scanf("%i",&N);
+  scanf("%c",&tipo);
+ 
+  if(tipo =='d'||tipo=='D'){
+    for(l=0;l<N;l++){
+      for(c=0;c<N;c++){
+        if(l==c){
+          printf("1 ");
+        }else{
+          printf("0 ");
+        }
+      }
+       printf("\n");
+    }
+  }
+  if(tipo=='s'||tipo=='C'){
+    for(l=0;l<N;l++){
+      for(c=0;c<N;c++){
+        if(l<=c){
+          printf("1 ");
+        }else{
+          printf("0 ");
+        }
+ 
+      }
+       printf("\n");
+    }
+  }
+  if(tipo=='i'||tipo=='I'){
+    for(l=0;l<N;l++){
+      for(c=0;c<N;c++){
+        if(l>=c){
+          printf("1 ");
+        }else{
+          printf("0 ");
+        }
+ 
+      }
+       printf("\n");
+    }
+  }
+  if(tipo=='t'||tipo=='T'){
+    for(l=0;l<N;l++){
+      for(c=0;c<N;c++){
+        if((l+c)%2==0){
+          printf("1 ");
+        }else{
+          printf("0 ");
+        }
+ 
+      }
+       printf("\n");
+    }
+  }
+return 0;
+}*/
